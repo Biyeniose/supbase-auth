51fa368f313750ef0ff1a445169e178e89be6b87
Biyeniose <biyeniose@gmail.com>
2024-08-23 15:30:36 -0400

    sign up + everything else seems to be working, only concern in mobile adapting

--- app/predict/page.tsx
+++ app/predict/page.tsx
@@ -1,3 +1,7 @@
 export default async function Predict() {
-  return <div>Predict Page</div>;
+  return (
+    <div className="flex flex-col items-center  min-h-screen">
+      <div className="text-center text-2xl mb-4 py-4">Predictions Page</div>
+    </div>
+  );
 }
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -51,8 +51,6 @@ export default async function Signup({
 
   return (
     <div>
-      <Header />
-
       <Link
         href="/"
         className="py-2 px-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover text-sm m-4"
